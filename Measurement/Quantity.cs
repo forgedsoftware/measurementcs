7d15aa0adaf95e1699005bf915b9b0aa442014e6
mharward <mharward@gmail.com>
2014-07-11 03:19:56 +1200

    - Add extended math functions

--- Measurement/Quantity.cs
+++ Measurement/Quantity.cs
@@ -117,11 +117,39 @@ namespace ForgedSoftware.Measurement {
 			throw new NotImplementedException();
 		}
 
-
-
 		#endregion
 
 		#region Extended Math
+
+		public Quantity Abs() { return new Quantity(Math.Abs(Value), Dimensions.CopyList()); }
+		public Quantity Acos() { return new Quantity(Math.Acos(Value), Dimensions.CopyList()); }
+		public Quantity Asin() { return new Quantity(Math.Asin(Value), Dimensions.CopyList()); }
+		public Quantity Atan() { return new Quantity(Math.Atan(Value), Dimensions.CopyList()); }
+		public Quantity Ceiling() { return new Quantity(Math.Ceiling(Value), Dimensions.CopyList()); }
+		public Quantity Cos() { return new Quantity(Math.Cos(Value), Dimensions.CopyList()); }
+		public Quantity Exp() { return new Quantity(Math.Exp(Value), Dimensions.CopyList()); }
+		public Quantity Floor() { return new Quantity(Math.Floor(Value), Dimensions.CopyList()); }
+		public Quantity Log() { return new Quantity(Math.Log(Value), Dimensions.CopyList()); }
+		public Quantity Log10() { return new Quantity(Math.Log10(Value), Dimensions.CopyList()); }
+		public Quantity Round() { return new Quantity(Math.Round(Value), Dimensions.CopyList()); }
+		public Quantity Sin() { return new Quantity(Math.Sin(Value), Dimensions.CopyList()); }
+		public Quantity Sqrt() { return new Quantity(Math.Sqrt(Value), Dimensions.CopyList()); }
+		public Quantity Tan() { return new Quantity(Math.Tan(Value), Dimensions.CopyList()); }
+
+		// Extra functions not avaliable in JS version
+		public Quantity Cosh() { return new Quantity(Math.Cosh(Value), Dimensions.CopyList()); }
+		public Quantity Sinh() { return new Quantity(Math.Sinh(Value), Dimensions.CopyList()); }
+		public Quantity Tanh() { return new Quantity(Math.Tanh(Value), Dimensions.CopyList()); }
+
+		// TODO: Add Quantity based version, check dimensions are equivalent
+		public Quantity Atan2(double y) { return new Quantity(Math.Atan2(y, Value), Dimensions.CopyList()); }
+		// TODO: Add Quantity based version, check dimensions are equivalent
+		public Quantity Pow(double y) { return new Quantity(Math.Pow(Value, y), Dimensions.CopyList()); }
+
+		// TODO: Add Quantity based version, check dimensions are equivalent
+		public Quantity Max(double y) { return new Quantity(Math.Max(Value, y), Dimensions.CopyList()); }
+		// TODO: Add Quantity based version, check dimensions are equivalent
+		public Quantity Min(double y) { return new Quantity(Math.Min(Value, y), Dimensions.CopyList()); }
 
 		#endregion
 
